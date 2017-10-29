4d019003f29800547f8169e9e9fffa8a9126f109
Damian <themrdamus@gmail.com>
2017-10-29 19:10:18 +0000

    diodes and start of segments

--- digital-clock.js
+++ digital-clock.js
@@ -2,14 +2,46 @@ let timer = 0;
 let diodes = [];
 window.onload = () => {
   diodes = document.getElementsByClassName("diode-wrapper")
-  setInterval(() => {
-    timer++
+  _refreshDiods()
+  // setInterval(() => {
+  //   timer++
+  //  _refreshDiods()
+  // }, 1000)
 
-  }, 1000)
-  $( "div.diode" ).toggleClass( "on" )
 };
+
+function onNumberInput(e){
+  const value = $('#decimal-input')[0].value
+  timer = value;
+  _refreshDiods()
+}
 
 function refreshDigits(){
   segments = $("div.segment")
-  let segments = [];  
-} 
+  let segments = [];
+  Array.from(segments).forEach(function(segment, i){
+    $(segments[i].getElementsByClassName('segment')[0]).removeClass( "on" )
+  }, this);
+  if ($(segments)[0]) {
+    
+  }
+}
+
+function _refreshDiods() {
+  Array.from(diodes).forEach(function(diode, i) {
+    $(diodes[i].getElementsByClassName('diode')[0]).removeClass( "on" )
+  }, this);
+  if(timer >= 8){
+    $(diodes[3].getElementsByClassName('diode')[0]).toggleClass( "on" )
+    timer -= 8;
+  } 
+  if(timer >= 4){
+    $(diodes[2].getElementsByClassName('diode')[0]).toggleClass( "on" )
+    timer -= 4;
+  } 
+  if(timer >= 2){
+    $(diodes[1].getElementsByClassName('diode')[0]).toggleClass( "on" )
+  } if(timer % 2){
+    $(diodes[0].getElementsByClassName('diode')[0]).toggleClass( "on" )
+  } 
+}
