a48d40b0ac779ad326c7e0dbbd18acc6776b1774
Damian <themrdamus@gmail.com>
2017-11-05 19:47:13 +0000

    displaying digits and timer

--- digital-clock.js
+++ digital-clock.js
@@ -1,47 +1,111 @@
 let timer = 0;
 let diodes = [];
+let digit = 0;
+let segments = [];
 window.onload = () => {
   diodes = document.getElementsByClassName("diode-wrapper")
   _refreshDiods()
-  // setInterval(() => {
-  //   timer++
-  //  _refreshDiods()
-  // }, 1000)
-
+  _refreshDigits()
+  setInterval(() => {
+    timer++
+    updateBoard(timer)
+  }, 1000)
 };
+
+function updateBoard(time){
+  _refreshDiods()
+  _refreshDigits()
+  _displayDigit(time)
+}
 
 function onNumberInput(e){
   const value = $('#decimal-input')[0].value
-  timer = value;
-  _refreshDiods()
+  updateBoard(value)
 }
 
-function refreshDigits(){
+function _refreshDigits(){
   segments = $("div.segment")
-  let segments = [];
   Array.from(segments).forEach(function(segment, i){
-    $(segments[i].getElementsByClassName('segment')[0]).removeClass( "on" )
+    $(segment).removeClass( "on" )
   }, this);
-  if ($(segments)[0]) {
+  // if ($(segments)[0]) {
     
-  }
+  // }
 }
 
 function _refreshDiods() {
+  let timeValue = timer
   Array.from(diodes).forEach(function(diode, i) {
     $(diodes[i].getElementsByClassName('diode')[0]).removeClass( "on" )
   }, this);
-  if(timer >= 8){
-    $(diodes[3].getElementsByClassName('diode')[0]).toggleClass( "on" )
-    timer -= 8;
+  if(timeValue >= 8){
+    $(diodes[3].getElementsByClassName('diode')[0]).addClass( "on" )
+    timeValue -= 8;
   } 
-  if(timer >= 4){
-    $(diodes[2].getElementsByClassName('diode')[0]).toggleClass( "on" )
-    timer -= 4;
+  if(timeValue >= 4){
+    $(diodes[2].getElementsByClassName('diode')[0]).addClass( "on" )
+    timeValue -= 4;
   } 
-  if(timer >= 2){
-    $(diodes[1].getElementsByClassName('diode')[0]).toggleClass( "on" )
-  } if(timer % 2){
-    $(diodes[0].getElementsByClassName('diode')[0]).toggleClass( "on" )
-  } 
+  if(timeValue >= 2){
+    $(diodes[1].getElementsByClassName('diode')[0]).addClass( "on" )
+  } if(timeValue % 2){
+    $(diodes[0].getElementsByClassName('diode')[0]).addClass( "on" )
+  }
+}
+
+function _displayDigit(timeValue){
+  digit = $(document.getElementsByClassName("digit"))
+  if(timeValue == 1) {
+    $('#topRight').addClass( "on" )&&
+    $('#botRight').addClass( "on" )
+  } else if (timeValue == 2) {
+    $('#top').addClass( "on" )&&
+    $('#topRight').addClass( "on" )&&
+    $('#middle').addClass( "on" )&&
+    $('#botLeft').addClass( "on" )&&
+    $('#bot').addClass( "on" )
+  } else if (timeValue == 3) {
+    $('#top').addClass( "on" )&&
+    $('#topRight').addClass( "on" )&&
+    $('#middle').addClass( "on" )&&
+    $('#botRight').addClass( "on" )&&
+    $('#bot').addClass( "on" )
+  } else if (timeValue == 4) {
+    $('#topLeft').addClass( "on" )&&
+    $('#topRight').addClass( "on" )&&
+    $('#middle').addClass( "on" )&&
+    $('#botRight').addClass( "on" )
+  } else if (timeValue == 5) {
+    $('#top').addClass( "on" )&&
+    $('#topLeft').addClass( "on" )&&
+    $('#middle').addClass( "on" )&&
+    $('#botRight').addClass( "on" )&&
+    $('#bot').addClass( "on" )
+  } else if (timeValue == 6) {
+    $('#top').addClass( "on" )&&
+    $('#topLeft').addClass( "on" )&&
+    $('#middle').addClass( "on" )&&
+    $('#botRight').addClass( "on" )&&
+    $('#bot').addClass( "on" ) &&
+    $('#botLeft').addClass( "on" )
+  } else if (timeValue == 7) {
+    $('#top').addClass( "on" )&&
+    $('#topRight').addClass( "on" )&&
+    $('#botRight').addClass( "on" )
+  } else if (timeValue == 8) {
+    $('#top').addClass( "on" )&&
+    $('#topLeft').addClass( "on" )&&
+    $('#topRight').addClass( "on" )&&
+    $('#middle').addClass( "on" )&&
+    $('#botRight').addClass( "on" )&&
+    $('#bot').addClass( "on" ) &&
+    $('#botLeft').addClass( "on" )
+  } else if (timeValue == 9) {
+    $('#top').addClass( "on" )&&
+    $('#topLeft').addClass( "on" )&&
+    $('#topRight').addClass( "on" )&&
+    $('#middle').addClass( "on" )&&
+    $('#botRight').addClass( "on" )&&
+    $('#bot').addClass( "on" )
+  }
 }
